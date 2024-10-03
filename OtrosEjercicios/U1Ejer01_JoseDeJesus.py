edfac0ef4a9972e4ef02e660126fb9ad753d6c37
jesusl12579 <jesuslucio01791@gmail.com>
2024-10-03 14:57:39 -0600

    cambios realizadoss

---
+++ OtrosEjercicios/U1Ejer01_JoseDeJesus.py
@@ -0,0 +1,44 @@
+# Nombre completo: Jose de Jesus Lucio Tovar
+# Fecha de creación: 3 de octubre 
+# Tema: Fundamentos de Python
+
+# Desplegar título Fundamentos de python
+print("Ejercicio 1 - Fundamentos de Python")
+
+# Realizar un salto de línea
+print()
+
+# variable tipo real igual a 4.0
+a = 2.0
+
+# variable tipo real igual a 3.0
+b = 2.0
+
+# Imprimir el contenido de las dos variables
+print("Variable a: " + str(a))
+print("Variable b: " + str(b))
+
+# Realizar un salto de línea
+print()
+
+# Hacer la suma de las dos variables
+suma = a + b
+
+#resta
+resta = a - b
+
+#--multiplicación
+multiplicacion = a * b
+
+#--división
+division = a / b
+
+#--potenciación
+potenciacion = a ** b
+
+# Imprimir los resultados
+print("Suma: " + str(suma))
+print("Resta: " + str(resta))
+print("Multiplicación: " + str(multiplicacion))
+print("División: " + str(division))
+print("Potenciación: " + str(potenciacion))
