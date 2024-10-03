8a8c5ca77112145f176e69650064277b7a73c515
jesusl12579 <jesuslucio01791@gmail.com>
2024-10-03 15:00:53 -0600

    ejercicios calis

---
+++ ejerciciosCalis/U1Ejer02_JoseDeJesus.py
@@ -0,0 +1,27 @@
+# Nombre completo: Jose de Jesus Lucio Tovar
+# Fecha de creación: 3 de octubre 
+# Tema: Fundamentos de Python
+
+# 1. Crear la lista con los nombres de los compañeros
+amigos = ["fabricio", "juanpa", "pablo", "ruben"]
+
+# 2. Mostrar el tamaño de la lista
+print("El tamaño de la lista es", len(amigos))
+
+# 3. Imprimir el contenido de la lista
+print("El contenido de la lista es", amigos)
+
+# 4. Cambiar el segundo elemento de la lista por "Joaquín"
+amigos[1] = "Joaquín"
+
+# 5. Agregar un elemento más a la lista con el nombre "Luis Miguel"
+amigos.append("Luis Miguel")
+
+# 6. Eliminar el tercer elemento de la lista
+del amigos[2]
+
+# 7. Desplegar la lista en orden inverso y separados por un carácter
+print("-".join(reversed(amigos)))
+
+# 8. Instrucción para un salto de línea
+print("\n")
